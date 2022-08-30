4f27e7dc9607aaddacd2a070f69cdc4b2924fc07
abdulkadirgul <abdulkadirgull1@gmail.com>
2022-08-30 15:16:17 +0300

    Solution Added / Layers added / Entites added / Migration Added.

---
+++ TravelReservationCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelReservationCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
