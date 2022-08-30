687313440c6f93be8f6cd126419ba43bdf2088bd
abdulkadirgul <abdulkadirgull1@gmail.com>
2022-08-30 23:35:10 +0300

    Fluent validation dependencies added to business layer \ Site theme folder added to wwwroot folder.

---
+++ TravelReservationCoreApp/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelReservationCoreApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
