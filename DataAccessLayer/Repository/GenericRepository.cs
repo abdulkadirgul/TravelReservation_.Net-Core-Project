1bc9dcc65751ef6fa7c1e2c18bc12826430e0b95
abdulkadirgul <abdulkadirgull1@gmail.com>
2022-08-30 15:43:10 +0300

    Generic repository added to data access layer.

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        public void Delete(T item)
+        {
+            using var c = new Context();
+            c.Remove(item);
+            c.SaveChanges();
+        }
+
+        public List<T> GetList()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+
+        }
+
+        public void Insert(T item)
+        {
+            using var c = new Context();
+            c.Add(item);
+            c.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            using var c = new Context();
+            c.Update(item);
+
+        }
+    }
+}
