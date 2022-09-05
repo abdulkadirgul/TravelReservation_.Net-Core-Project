d951919594532b7e542d148172bd6e98772a83f0
abdulkadirgul <abdulkadirgull1@gmail.com>
2022-09-05 23:30:19 +0300

    View components and partial layouts added/arranged.

---
+++ TravelReservationCoreApp/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelReservationCoreApp.ViewComponents.Default
+{
+    public class _PopularDestinations : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View();
+        }
+    }
+}
---
+++ TravelReservationCoreApp/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelReservationCoreApp.Views.Shared.Components._SliderPartial
+{
+    public class _SliderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
