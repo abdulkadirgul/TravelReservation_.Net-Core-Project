cc2aab55527de3278f291f81d6edb38b7ef97aae
abdulkadirgul <abdulkadirgull1@gmail.com>
2022-12-22 12:08:47 +0300

    When SignUp : Password warnings convert to  Turkish wit using Identity Error Describer Class.

---
+++ TravelReservationCoreApp/Models/CustomIdentityValidator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TravelReservationCoreApp.Models
+{
+    public class CustomIdentityValidator:IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError
+            {
+                Code = "PasswordTooShort",
+                Description=$"Parola minimum {length} karakter olmalıdır."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code = "PasswordRequiresUpper",
+                Description = $"Parola En Az 1 Büyük Karakter İçermelidir."
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = "PasswordRequiresLower",
+                Description = $"Parola En Az 1 Küçük Karakter İçermelidir."
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = $"Parola En Az 1 Sembol Karakter İçermelidir."
+            };
+        }
+
+    }
+}
